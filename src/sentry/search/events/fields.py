79d31dfa6e7b31884ed4cc94fb785f6e69304c64
GerevSec <ayalmrg@gmail.com>
2021-09-10 19:16:59 +0300

    aaa

--- src/sentry/search/events/fields.py
+++ src/sentry/search/events/fields.py
@@ -2175,9 +2175,58 @@ class QueryFields(QueryBase):
                                 ],
                             )
                         ],
-                        alias,
+def __init__(self, dataset: Dataset, params: ParamsType):
                     ),
                     default_result_type="integer",
+                ),
+                SnQLFunction(
+                    "apdex",
+                    optional_args=[NullableNumberRange("satisfaction", 0, None)],
+                    snql_aggregate=self._resolve_apdex_function,
+                    default_result_type="number",
+                ),
+                SnQLFunction(
+                    "count_miserable",
+                    # Using the generic FunctionArg here temporarily till we
+                    # implement a resolver for count columns in SnQL. The only
+                    # column to be passed through here for now is `user`.
+                    required_args=[FunctionArg("column")],
+                    optional_args=[NullableNumberRange("satisfaction", 0, None)],
+                    calculated_args=[
+                        {
+                            "name": "tolerated",
+                            "fn": lambda args: args["satisfaction"] * 4.0
+                            if args["satisfaction"] is not None
+                            else None,
+                        }
+                    ],
+                    snql_aggregate=self._resolve_count_miserable_function,
+                    default_result_type="integer",
+                ),
+                SnQLFunction(
+                    "user_misery",
+                    # To correct for sensitivity to low counts, User Misery is modeled as a Beta Distribution Function.
+                    # With prior expectations, we have picked the expected mean user misery to be 0.05 and variance
+                    # to be 0.0004. This allows us to calculate the alpha (5.8875) and beta (111.8625) parameters,
+                    # with the user misery being adjusted for each fast/slow unique transaction. See:
+                    # https://stats.stackexchange.com/questions/47771/what-is-the-intuition-behind-beta-distribution
+                    # for an intuitive explanation of the Beta Distribution Function.
+                    optional_args=[
+                        NullableNumberRange("satisfaction", 0, None),
+                        with_default(5.8875, NumberRange("alpha", 0, None)),
+                        with_default(111.8625, NumberRange("beta", 0, None)),
+                    ],
+                    calculated_args=[
+                        {
+                            "name": "tolerated",
+                            "fn": lambda args: args["satisfaction"] * 4.0
+                            if args["satisfaction"] is not None
+                            else None,
+                        },
+                        {"name": "parameter_sum", "fn": lambda args: args["alpha"] + args["beta"]},
+                    ],
+                    snql_aggregate=self._resolve_user_misery_function,
+                    default_result_type="number",
                 ),
                 SnQLFunction(
                     "count",
@@ -2221,18 +2270,12 @@ class QueryFields(QueryBase):
                 SnQLFunction("p50", snql_aggregate=self._resolve_unimplemented_function),
                 SnQLFunction("p75", snql_aggregate=self._resolve_unimplemented_function),
                 SnQLFunction("p95", snql_aggregate=self._resolve_unimplemented_function),
-                SnQLFunction("p99", snql_aggregate=self._resolve_unimplemented_function),
+def __init__(self, dataset: Dataset, params: ParamsType):
                 SnQLFunction("p100", snql_aggregate=self._resolve_unimplemented_function),
                 SnQLFunction("eps", snql_aggregate=self._resolve_unimplemented_function),
                 SnQLFunction("epm", snql_aggregate=self._resolve_unimplemented_function),
-                SnQLFunction("apdex", snql_aggregate=self._resolve_unimplemented_function),
-                SnQLFunction(
-                    "count_miserable", snql_aggregate=self._resolve_unimplemented_function
-                ),
-                SnQLFunction("user_misery", snql_aggregate=self._resolve_unimplemented_function),
                 SnQLFunction("array_join", snql_aggregate=self._resolve_unimplemented_function),
                 SnQLFunction("histogram", snql_aggregate=self._resolve_unimplemented_function),
-                SnQLFunction("count_unique", snql_aggregate=self._resolve_unimplemented_function),
                 SnQLFunction("count_at_least", snql_aggregate=self._resolve_unimplemented_function),
                 SnQLFunction("min", snql_aggregate=self._resolve_unimplemented_function),
                 SnQLFunction("max", snql_aggregate=self._resolve_unimplemented_function),
@@ -2252,10 +2295,11 @@ class QueryFields(QueryBase):
                 SnQLFunction("count_range", snql_aggregate=self._resolve_unimplemented_function),
                 SnQLFunction("percentage", snql_aggregate=self._resolve_unimplemented_function),
                 SnQLFunction("t_test", snql_aggregate=self._resolve_unimplemented_function),
-                SnQLFunction("minus", snql_aggregate=self._resolve_unimplemented_function),
+def __init__(self, dataset: Dataset, params: ParamsType):
                 SnQLFunction(
                     "absolute_correlation", snql_aggregate=self._resolve_unimplemented_function
                 ),
+                SnQLFunction("count_unique", snql_aggregate=self._resolve_unimplemented_function),
                 SnQLFunction("count_if", snql_aggregate=self._resolve_unimplemented_function),
                 SnQLFunction(
                     "compare_numeric_aggregate", snql_aggregate=self._resolve_unimplemented_function
@@ -2358,10 +2402,13 @@ class QueryFields(QueryBase):
             raise InvalidSearchQuery(f"Invalid characters in field {function}")
 
         if function in self.params.get("aliases", {}):
-            raise NotImplementedError("Aggregate aliases not implemented in snql field parsing yet")
+def resolve_function(self, function: str, match: Optional[Match[str]] = None) ->
 
         name, arguments, alias = self.parse_function(match)
         snql_function = self.function_converter.get(name)
+
+        arguments = snql_function.format_as_arguments(name, arguments, self.params)
+
         if snql_function.snql_aggregate is not None:
             self.aggregates.append(snql_function.snql_aggregate(arguments, alias))
         return snql_function.snql_aggregate(arguments, alias)
@@ -2611,8 +2658,105 @@ class QueryFields(QueryBase):
     def _resolve_error_handled_alias(self, _: str) -> SelectType:
         # Columns in snuba doesn't support aliasing right now like Function does.
         # Adding a no-op here to get the alias.
-        return Function(
+def _resolve_error_handled_alias(self, _: str) -> SelectType:
             "cast", [self.column("error.handled"), "Array(Nullable(UInt8))"], ERROR_HANDLED_ALIAS
+        )
+
+    def _project_threshold_multi_if_function(self) -> SelectType:
+        """Accessed by `_resolve_apdex_function` and `_resolve_count_miserable_function`,
+        this returns the right duration value (for example, lcp or duration) based
+        on project or transaction thresholds that have been configured by the user.
+        """
+
+        return Function(
+            "multiIf",
+            [
+                Function(
+                    "equals",
+                    [
+                        Function(
+                            "tupleElement",
+                            [self.resolve_field("project_threshold_config"), 1],
+                        ),
+                        "lcp",
+                    ],
+                ),
+                self.column("measurements.lcp"),
+                self.column("transaction.duration"),
+            ],
+        )
+
+    def _resolve_apdex_function(self, args: Mapping[str, str], alias: str) -> SelectType:
+        if args["satisfaction"]:
+            function_args = [self.column("transaction.duration"), int(args["satisfaction"])]
+        else:
+            function_args = [
+                self._project_threshold_multi_if_function(),
+                Function("tupleElement", [self.resolve_field("project_threshold_config"), 2]),
+            ]
+
+        return Function("apdex", function_args, alias)
+
+    def _resolve_count_miserable_function(self, args: Mapping[str, str], alias: str) -> SelectType:
+        if args["satisfaction"]:
+            lhs = self.column("transaction.duration")
+            rhs = int(args["tolerated"])
+        else:
+            lhs = self._project_threshold_multi_if_function()
+            rhs = Function(
+                "multiply",
+                [
+                    Function(
+                        "tupleElement",
+                        [self.resolve_field("project_threshold_config"), 2],
+                    ),
+                    4,
+                ],
+            )
+        col = args["column"]
+
+        return Function(
+            "uniqIf",
+            [
+                self.resolve_field_alias(col) if self.is_field_alias(col) else self.column(col),
+                Function("greater", [lhs, rhs]),
+            ],
+            alias,
+        )
+
+    def _resolve_user_misery_function(self, args: Mapping[str, str], alias: str) -> SelectType:
+        if args["satisfaction"]:
+            count_miserable_agg = self.resolve_function(
+                f"count_miserable(user,{args['satisfaction']})"
+            )
+        else:
+            count_miserable_agg = self.resolve_function("count_miserable(user)")
+
+        return Function(
+            "ifNull",
+            [
+                Function(
+                    "divide",
+                    [
+                        Function(
+                            "plus",
+                            [
+                                count_miserable_agg,
+                                args["alpha"],
+                            ],
+                        ),
+                        Function(
+                            "plus",
+                            [
+                                Function("uniq", [self.column("user")]),
+                                args["parameter_sum"],
+                            ],
+                        ),
+                    ],
+                ),
+                0,
+            ],
+            alias,
         )
 
     def _resolve_unimplemented_function(
